ecdf01ee64313c848bde91efd5a4d59fa4e2dd9b
Sai Chimata <27644730+sai-chai@users.noreply.github.com>
2020-01-06 03:36:05 -0600

    Updates

---
+++ precache-manifest.351594815c37fcf9c285151a90b928c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e949add1b9ef61011279f107bf67cda3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dbfa992c9a338baf40c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d4349af346be2c1b721e",
+    "url": "/static/js/2.bc92bb8f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.bc92bb8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7dbfa992c9a338baf40c",
+    "url": "/static/js/main.4dce406d.chunk.js"
+  },
+  {
+    "revision": "2dc8f9d1f76d42ab8602",
+    "url": "/static/js/runtime-main.4df33391.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
